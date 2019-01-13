3e35a71f88d2ccb20b4e5c2c1e8d4efca932efb6
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-13 19:28:38 +0500

    socketio messenger all needs

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ app.get('/socketio', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
     });
   });
+
+  io.emit('some event', { for: 'everyone' });
+
 
 
 app.get('/', function(req, res){
