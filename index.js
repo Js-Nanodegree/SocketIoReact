5aa7555941997c2e61b13a7e8a808049f7b698eb
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-13 19:21:46 +0500

    a user connected

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+var io = require('socket.io')(http);
+
+app.get('/socketio', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
