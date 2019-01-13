bb710108c0126f1762ae8179734041d23a993ecf
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-13 19:58:05 +0500

    new visitor socket.io

--- index.js
+++ index.js
@@ -3,28 +3,38 @@ var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
+const port =3000
+
 app.get('/socketio', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    console.log('user connect')
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);
     });
+    socket.on("new_visitor", user => {
+        console.log("new_visitor", user);
+        socket.user = user;
+      });
+    socket.on('disconnect',function(){
+        console.log('user disconnect')
+    })
   });
 
   io.emit('some event', { for: 'everyone' });
 
 
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
 
 app.get('*',function (req,res){
     res.send('<h1>PAGE DONT FIND </h1>')
 })
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+  });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  http.listen(port, function() {
+    console.log(`listening on *:${port}`);
+  });
