eb08e27fdca41f4d42cab8b2503a5053d1556b5a
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-20 20:00:57 +0500

    Socketio React Connected

--- src/Component/Layout.js
+++ src/Component/Layout.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import io from 'socket.io-client'
 
+const socketUrl = "http://localhost:4020/"
 export default class Layout extends React.Component{
     
     constructor(props){
         super(props)
-        this.state ={}
+        this.state ={
+            socket: null
+        }
+    }
+
+    componentDidMount(){
+        this.initSocket()
+    }
+
+    initSocket =() =>{
+        const socket = io(socketUrl)
+        socket.on('connect',()=>{
+            console.log('connected')
+        })
+        this.setState({socket})
     }
     
     render(){
