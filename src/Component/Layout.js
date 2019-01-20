be7a7c208121824e4f5c4ae650d24d1b90f70bdb
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-20 19:51:14 +0500

    State reactjs init

--- src/Component/Layout.js
+++ src/Component/Layout.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default class Layout extends React.Component{
+    
+    constructor(props){
+        super(props)
+        this.state ={}
+    }
+    
     render(){
         const {title} =this.props
         return(
