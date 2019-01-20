c01c52b66ca03967b5d05ba05167021c0e72a1da
js-nanodegree@outlook.com <js-nanodegree@outlook.com>
2019-01-21 02:01:37 +0500

    Initial form

--- src/Component/chatContainer.js
+++ src/Component/chatContainer.js
@@ -3,16 +3,32 @@ import SideBar from './SideBar'
 
 export default class extends React.Component{
     constructor(props){
+        super(props)
         this.state={
             chats:[],
             activeChat:null
         }
     }
+
+    sendMessage=(chatId,message)=>{
+        const {socket} = this.props
+        socket.emit(MESAGE_SEND, {chatId,message})
+
+    }
+
+    sendTyping=(chatId,isTyping)=>{
+        const{socket}=this.props
+        socket.emit(TYPING,{chatId,isTyping})
+
+    }
+
+
     setActivechat =(activeChat)=>{
         this.setState({activeChat})
     }
     render(){
         const {user,logout} = this.props
+        const {chats,activeChat} =this.state
         return(
             <div>
                 <SideBar 
